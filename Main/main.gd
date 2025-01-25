6c1cc3ed55eb110ac318ba1be7fe054f8eaa41bd
emib <emilianobarg@gmail.com>
2025-01-25 09:09:17 -0300

    no me acuerdo que hicimos

---
+++ Main/main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+@export var Vein : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var StartingVein = Vein.instantiate()
+	add_child(StartingVein)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
