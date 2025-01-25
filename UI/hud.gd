5cf9a0ba7dc663c45c325d5d792a5e8b1da39b6c
Naiko2004 <nicolasantiagomez4@gmail.com>
2025-01-25 11:03:06 -0300

    HUD init y HP

---
+++ UI/hud.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+signal start_game()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func changeHP(newHP : int):
+	$TemporalHP.text = "HP: " + str(newHP)
+	pass
