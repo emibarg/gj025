f3a12a4c776ebba9ebee16a81e1bc44a23050eab
emib <emilianobarg@gmail.com>
2025-01-24 21:19:08 -0300

    enemy first  try

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
+var gravity :int = 10
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	##Spawn on top of the screen
+	$RigidBody2D.position.y = -100
+	$RigidBody2D.position.x = randf_range(0, 800)
+	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+
+	##Apply gravity
+	$RigidBody2D.position.y += gravity * delta
