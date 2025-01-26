864d482a5ad33083d4015c9b30bcb1d3f2126492
Naiko2004 <nicolasantiagomez4@gmail.com>
2025-01-26 13:01:58 -0300

    jeringas como HP listo

--- UI/hud.gd
+++ UI/hud.gd
@@ -4,6 +4,9 @@ signal start_game()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$HP1.texture = load("res://UI/vaccine full.png")
+	$HP2.texture = load("res://UI/vaccine full.png")
+	$HP3.texture = load("res://UI/vaccine full.png")
 	pass # Replace with function body.
 
 
@@ -13,6 +16,12 @@ func _process(delta):
 
 func changeHP(newHP : int):
 	$TemporalHP.text = "HP: " + str(newHP)
+	if newHP == 2:
+		$HP3.texture = load("res://UI/vaccine empty.png")
+	if newHP == 1:
+		$HP2.texture = load("res://UI/vaccine empty.png")
+	if newHP == 0:
+		$HP1.texture = load("res://UI/vaccine empty.png")
 	pass
 
 
