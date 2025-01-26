9ecc8846210c2b297f8b2dde90dc90a18879613d
Naiko2004 <nicolasantiagomez4@gmail.com>
2025-01-26 12:43:46 -0300

    sound effects ends

---
+++ endings/ded.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(0.3).timeout
+	$Beeep.play(2.1)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_beeep_finished():
+	pass # Replace with function body.
---
+++ endings/flatulencius.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(0.3).timeout
+	$WetFart.play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
