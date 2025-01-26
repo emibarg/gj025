c2c5c244888e87628b7d7805294e8e9804924b96
emib <emilianobarg@gmail.com>
2025-01-26 14:31:21 -0300

    introsfx

--- intro/nextintrocontrol.gd
+++ intro/nextintrocontrol.gd
@@ -1,17 +1,15 @@
-extends Button
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body
-
+	$Button2.hide()
+	$BELIEVEME.play()
+	await get_tree().create_timer(3).timeout
+	$Button2.show()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://intro/nextintro.tscn")
-	pass # Replace with function body.
