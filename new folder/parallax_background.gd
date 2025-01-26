33538abaada8e82b63e722a0180ff1bcc368c430
emib <emilianobarg@gmail.com>
2025-01-25 23:10:58 -0300

    parallax

--- new folder/parallax_background.gd
+++ new folder/parallax_background.gd
@@ -1,5 +1,5 @@
 extends ParallaxBackground
-
+@export var active : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +8,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	scroll_base_offset.y += 200 * delta
+	if active:
+		scroll_base_offset.y += 200 * delta
+	else:
+		pass
+
